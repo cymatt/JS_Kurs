a74802dbcda2fe3c46683dbe58f7652e6de8a7ae
cymatt <cy.matthias@gmail.com>
2021-12-26 17:27:36 +0100

    Third commit

--- 10-Mathematik.js
+++ 10-Mathematik.js
@@ -1,2 +1,13 @@
 "use strict"
 
+// Addition
+console.log ("5 + 6:", 5 + 6)
+
+// Subtraktion
+console.log ("5 - 6:", 5 - 6)
+
+//Multiplikation
+console.log ("5 * 6:", 5 * 6)
+
+// Division
+console.log ("5 / 6:", 5 / 6)
